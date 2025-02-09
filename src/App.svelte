2482bb2e4bb63778132b52bf0657321dcdf81ffd
Hayat Ebrahim <ebrahimhayat241@gmail.com>
2025-02-09 12:51:14 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,4 @@
+<script>
+      import Home  from './home.svelte';
+</script>
+<Home/>
---
+++ src/home.svelte
@@ -0,0 +1,12 @@
+
+<!-- <script>
+   function Home() {}
+</script>
+
+<div>
+  <h1>Home</h1>
+  <p>Welcome to the home page</p>
+</div>
+ -->
+
+ <p>HOME</p>
