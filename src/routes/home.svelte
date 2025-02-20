2d241a204c7a01e644dd27447eb78a6e6ca0cce0
Hayat Ebrahim <ebrahimhayat241@gmail.com>
2025-02-20 21:51:20 +0300

    home page finishing

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -16,44 +16,69 @@ function Home() {
   </span>
 </div>
 <div>
-  <div>
-  <h2 class="text-xl font text-gray-500">WElCOME TO </h2>
+  <div class="flex justify-between">
+  <div class="px-20 py-20">
+  <h2 class="text-xl font text-gray-500">WELCOME TO </h2>
   <h1 class="text-4xl font-bold text-gray-600">H O T E L</h1>
   <div class="text-gray-600" >
     <p>Book Your Stay And enjoy Luxury </p>
-    <p>redefiend at the most affordable rates</p>
+    <p>Redefiend at the most affordable rates , </p>
   </div>
 
 </div>
-<div>
+<div class="">
   <img src="/hotel.jpg" alt="tnidjsfnmas" />
 </div>
-<div>
-  <button>Book now</button>
+ </div>
+  
+<div class="flex justify-center"  >
+  <button class="bg-blue-500 text-white px-10 py-5 rounded-xl">Book now</button>
 </div>
-<p>All our rooms types are including the complemntary breakfast</p>
+<p class="flex justify-center text-xl py-7">All our rooms types are including the complementary breakfast</p>
 <div>
-  <div>
-    <h1>Luxury Redefined</h1>
-    <p>our rooms are designed to transport  you in to an enviroment made for leisure . take your mind off the day to day home life and find privateparadise for yourself</p>
-    <img src="/images.jpg" alt="sdf" />
-    <button>Explore</button>
+  <div class="flex justify-between">
+    <div class= "px-10">
+    <h1 class="text-3xl font-bold font-sans italic">Luxury Redefined</h1>
+    <p class="py-3">our rooms are designed to transport  you <br/>
+      into an enviroment made for leisure . <br/>
+      take your mind off the day to day home <br/>
+      life and find privateparadise for yourself</p>
+    <button class="bg-blue-500 text-white rounded-xl px-15 py-2">Explore</button>
   </div>
-  <div>
-    <h1>
-      Leave your Worries in the sand
-    </h1>
-    <p>we love life at  the beach . being close to ocean with acces to endless sandybeach ensures  a relaxed state of mind. it seamed like times stand still watching the ocean.</p>
-    <img src="/images (1).jpg" alt="sdf" />
-    <button>Explore</button>
+    <div>
+      <img src="/images.jpg" alt="sdf" />
+    </div>
+
   </div>
-  <div>
-    <h1>
+  
+  <div class="flex justify-between px-10">
+      <div>
+        <h1  class="text-3xl font-bold font-sans italic py-5">
+        Leave your Worries in <br/>the sand
+      </h1>
+      <p>we love life at the beach.<br/>
+        being close to ocean with access to endless sandy <br/>
+        beach ensures a relaxed state of mind .<br/>
+        it seamed like times stand still watching the <br/>
+        ocean.</p>
+        <button class="bg-blue-500 rounded-xl text-white px-15 py-2">Explore</button> 
+    </div>
+    <div >
+      <img src="/images (1).jpg" alt="sdf"/>
+      
+    </div>  
+  </div>
+  <div >
+    <h1 class="flex justify-center text-3xl font-bold font-sans italic py-5">
       Testimonials
     </h1>
-    <p>"Calm , serene , retro - What away to relax and enjoy"</p><p>Mr. and Mrs. Baxetry, Uk </p>
-    <buton> </buton><buton> > </buton>
-
+    <p class="text-center">"Calm , serene , retro - What away to relax and enjoy"</p>
+    <p class="text-center">Mr. and Mrs. Baxetry, Uk </p>
+    <div class="flex justify-center gap-2">
+      <button class="bg-blue-500 text-white px-2 rounded"> &lt; </button>
+      <button class="bg-blue-500 text-white px-2  rounded"> &gt; </button>
+    </div>
+    
   </div>
 </div>
 
